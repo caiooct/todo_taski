7a7475c4a2def8fb86ee6be2b9c4a89ee443c5b2
caiooctavio <pkaiow@gmail.com>
2025-02-04 22:35:47 -0400

    add assets

---
+++ lib/config/assets.dart
@@ -0,0 +1,11 @@
+abstract final class Assets {
+  static const createIcon = 'assets/svg/create_icon.svg';
+  static const doneIcon = 'assets/svg/done_icon.svg';
+  static const editIcon = 'assets/svg/edit_icon.svg';
+  static const logo = 'assets/png/logo.png';
+  static const noData = 'assets/svg/no_data.svg';
+  static const profile = 'assets/png/profile.png';
+  static const removeIcon = 'assets/svg/remove_icon.svg';
+  static const searchIcon = 'assets/svg/search_icon.svg';
+  static const todoIcon = 'assets/svg/todo_icon.svg';
+}
