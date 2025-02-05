0f49ce093780a8da3e99ecc8c2c675c0b33acc3f
caiooctavio <pkaiow@gmail.com>
2025-02-04 22:39:31 -0400

    add colors

---
+++ lib/config/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const blue = Color(0xFF007FFF);
+  static const blue10 = Color(0x1A007FFF);
+  static const fireRed = Color(0xFFFF5E5E);
+  static const mutedAzure = Color(0xFFC6CFDC);
+  static const paleWhite = Color(0xFFF5F7F9);
+  static const paleWhite50 = Color(0xCCC6CFDC);
+  static const redShade = Color(0xFFFFB4AB);
+  static const slateAzure = Color(0xFF8D9CB8);
+  static const slateBlue = Color(0xFF8D9CB8);
+  static const slatePurple = Color(0xFF3F3D56);
+}
